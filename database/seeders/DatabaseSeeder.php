441419873995c569b84453af1752d02cfd8229ad
seiis <seiisland@gmail.com>
2025-01-21 03:41:59 +0900

    シーダーファイル登録

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        //シーダークラス呼び出し処理
+        if (config('app.env') == 'local') {
+            $this->call(TaskSeeder::class);
+        }
     }
 }
--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TaskSeeder extends Seeder
 {
@@ -13,5 +14,11 @@ class TaskSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('tasks')->insert([
+            'title' => 'テストタスク1',
+            'body' => 'テストタスク1の内容です。',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
     }
 }
