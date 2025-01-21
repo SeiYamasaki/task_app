d350d7dc9dfd438288cf1e55fd4eff9c9ba4d49a
seiis <seiisland@gmail.com>
2025-01-21 15:30:42 +0900

    微修正完了

--- app/Http/Requests/TaskRequest.php
+++ app/Http/Requests/TaskRequest.php
@@ -11,7 +11,7 @@ class TaskRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return True;
+        return true;
     }
 
     /**
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -33,9 +33,7 @@
             </p>
             <p>
                 <label for="body">本文</label><br>
-
                 <textarea name="body" class="body" id="body">{{ $task->body }}</textarea>
-
             </p>
             <input type="submit" value="更新">
             <input type="button" onclick="location.href='/tasks/{{ $task->id }}'" value="詳細へ戻る">
